1126a7193d9e76bc36a70ece42da280827fa462a
Harsh <harshgajjar145@gmail.com>
2024-06-28 11:54:35 +0530

    fea: basic setup

---
+++ src/components/Hero.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="relative overflow-hidden before:absolute before:top-0 before:start-1/2 before:bg-[url('https://preline.co/assets/svg/examples/squared-bg-element.svg')] before:bg-no-repeat before:bg-center before:size-full before:-z-[1] before:transform before:-translate-x-1/2 min-h-screen flex justify-center items-center">
+      <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 pt-24 pb-10">
+        <div className="flex justify-center">
+          <a
+            className="inline-flex items-center gap-x-2 bg-white border border-gray-200 text-xs text-gray-600 p-2 px-3 rounded-full transition hover:border-gray-300"
+            href="#"
+          >
+            Explore the Campaigns
+            <span className="flex items-center gap-x-1">
+              <span className="border-s border-gray-200 text-blue-600 ps-2">
+                Explore
+              </span>
+              <svg
+                className="flex-shrink-0 size-4 text-blue-600"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="m9 18 6-6-6-6" />
+              </svg>
+            </span>
+          </a>
+        </div>
+
+        <div className="mt-5 max-w-xl text-center mx-auto">
+          <h1 className="block font-bold text-gray-800 text-4xl md:text-5xl lg:text-6xl">
+            Supercharged Preline Experience
+          </h1>
+        </div>
+
+        <div className="mt-5 max-w-3xl text-center mx-auto">
+          <p className="text-lg text-gray-600">
+            Preline is a large open-source project, crafted with Tailwind CSS
+            framework by Hmlstream.
+          </p>
+        </div>
+        <div className="mt-8 gap-3 flex justify-center">
+          <a
+            className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-full py-3 px-4"
+            href="#"
+          >
+            Connect wallet
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,39 @@
 import "@/styles/globals.css";
+import { OnchainKitProvider } from "@coinbase/onchainkit";
+import "@coinbase/onchainkit/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { base } from "viem/chains";
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { baseSepolia } from "wagmi/chains";
+import { coinbaseWallet } from "wagmi/connectors";
+
+const config = createConfig({
+  chains: [baseSepolia],
+  connectors: [
+    coinbaseWallet({
+      appName: "onchainkit",
+    }),
+  ],
+  ssr: true,
+  transports: {
+    [baseSepolia.id]: http(),
+  },
+});
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <OnchainKitProvider
+          apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_KEY}
+          chain={base}
+        >
+          <Component {...pageProps} />
+        </OnchainKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }

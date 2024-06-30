daabe63468b6cef0cb560d9add222c16fa6e78e3
Harsh <harshgajjar145@gmail.com>
2024-06-30 20:09:00 +0530

    chore: get started link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -53,7 +53,7 @@ export default function Hero() {
           <Button
             className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-full py-3 px-4"
             onClick={() => {
-              router.push("/events");
+              router.push("/register");
             }}
           >
             Get Started
